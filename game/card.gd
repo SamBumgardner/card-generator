31765b54fea35689ec945f266b28833a16c74799
Sam Bumgardner <Sambumgardner1@gmail.com>
2025-04-20 16:04:46 -0500

    implement card sheet image capture
    Tested it with 'gear' cards for a work-in-progress board game.

--- game/card.gd
+++ game/card.gd
@@ -1,8 +1,17 @@
-extends MarginContainer
+class_name Card extends MarginContainer
 
-@export var cost: int = 0
-@export var card_name: String = "Luke's Card"
+@export var card_spec: CardSpecification
 
-func _ready():
-    $ColorRect/CostIcon/CostValue.text = str(cost)
-    $ColorRect/Name.text = card_name   
+@onready var name_label: Label = $ColorRect/Header/Name
+@onready var supertype_label: Label = $ColorRect/Header/Supertype
+@onready var typeline_label: Label = $ColorRect/Body/Typeline
+@onready var effect_label: Label = $"ColorRect/Body/EffectContainer/Effect"
+@onready var costvalue_label: Label = $ColorRect/CostIcon/CostValue
+
+func _ready() -> void:
+    if card_spec != null:
+        name_label.text = card_spec.fields['name']
+        supertype_label.text = card_spec.supertype
+        typeline_label.text = card_spec.fields['typeline']
+        effect_label.text = card_spec.fields['effect']
+        costvalue_label.text = card_spec.fields['gear_token_cost']
--- game/card_generator.gd
+++ game/card_generator.gd
@@ -1,6 +1,77 @@
 extends Control
 
+@export var set_to_print: CardPrintingSet
+@export var cards_per_page: int = 9
+@export var cards_per_row: int = 3
+
+var current_print_spec_i: int = 0
+var current_card_printed_count: int = 0
+var print_page_number = 0
+
+var skipped_first_frame: bool = false
+
+var waiting: bool = false
+
 func _ready():
-    DisplayServer.window_set_size(Vector2i($GridContainer.size))
-    await RenderingServer.frame_post_draw
-    get_viewport().get_texture().get_image().save_png("res://new_card.png")
+    $GridContainer.columns = cards_per_row
+
+func get_next_page_of_card_specs() -> Array:
+    var next_page = []
+    var printing_specs = set_to_print.printing_specs
+    while current_print_spec_i < printing_specs.size():
+        var current_card = printing_specs[current_print_spec_i]
+        while current_card_printed_count < current_card.print_count:
+            next_page.append(current_card.card_spec)
+            current_card_printed_count += 1
+
+            if next_page.size() >= cards_per_page:
+                return next_page
+        
+        current_print_spec_i += 1
+        current_card_printed_count = 0
+    
+    return next_page
+
+func _process(_delta: float) -> void:
+    if waiting:
+        return
+    
+    var next_page = get_next_page_of_card_specs()
+    if not next_page.is_empty():
+        _instantiate_cards(next_page)
+    else:
+        get_tree().quit()
+        return
+    
+    if not skipped_first_frame:
+        waiting = true
+        await RenderingServer.frame_post_draw
+        DisplayServer.window_set_size(Vector2i(2475, 3535))
+        print("first frame has been drawn")
+        skipped_first_frame = true
+        waiting = false
+        current_print_spec_i = 0
+        current_card_printed_count = 0
+        print_page_number = 0
+        return
+    else:
+        waiting = true
+        print("starting wait for page %s" % print_page_number)
+        await RenderingServer.frame_post_draw
+        print("wait finished for page %s" % print_page_number)
+        waiting = false
+        capture_card_sheet_image()
+
+func _instantiate_cards(next_page_of_cards: Array):
+    if not next_page_of_cards.is_empty():
+        $GridContainer.get_children().map(func(x): x.queue_free())
+        for spec in next_page_of_cards:
+            var card_scene: PackedScene = load("res://card.tscn")
+            var card: Card = card_scene.instantiate()
+            card.card_spec = spec
+            $GridContainer.add_child(card)
+
+func capture_card_sheet_image():
+    get_viewport().get_texture().get_image().save_png("res://data/cards/print_sheet_%s.png" % print_page_number)
+    print("image captured for page %s" % print_page_number)
+    print_page_number += 1
